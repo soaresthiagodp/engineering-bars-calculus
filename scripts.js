ddef8c35eb5eff077bbb0a14b3ef6c7a148e149d
Thiago <soaresthiagodp@gmail.com>
2024-09-15 01:20:02 -0300

    Dark Mode

--- scripts.js
+++ scripts.js
@@ -143,3 +143,21 @@ document.getElementById('formula-select').addEventListener('change', function ()
 
 // Initialize to show the first section by default
 document.getElementById('formula-select').dispatchEvent(new Event('change'));
+
+document.getElementById("dark-mode-toggle").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+    this.classList.toggle("dark-mode");
+
+    // Salva a preferência no localStorage
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("dark-mode", "enabled");
+    } else {
+        localStorage.setItem("dark-mode", "disabled");
+    }
+});
+
+// Verifica a preferência do usuário e aplica o modo escuro se estiver habilitado
+if (localStorage.getItem("dark-mode") === "enabled") {
+    document.body.classList.add("dark-mode");
+    document.getElementById("dark-mode-toggle").classList.add("dark-mode");
+}
